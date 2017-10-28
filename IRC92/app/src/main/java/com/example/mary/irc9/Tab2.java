8202000c62719dee94f9c5dcafb58a2a2b6bd2e3
ryangulati <ryangulati@gmail.com>
2017-10-28 05:56:46 -0400

    Updated tabs

--- IRC92/app/src/main/java/com/example/mary/irc9/Tab2.java
+++ IRC92/app/src/main/java/com/example/mary/irc9/Tab2.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -25,13 +27,22 @@ public class Tab2 extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab2, container, false);
 
+        String[] notifs = {"[local place] Police: 123-456-7890",
+                "[local place] Hospital: 123-456-7890", "IRC [place] Office Contact: 123-456-7890"};
 
+        ListView lView = (ListView) rootView.findViewById(R.id.notifs);
+
+        ArrayAdapter<String> lViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, notifs);
+
+        lView.setAdapter(lViewAdapter);
+
+        return rootView;
 
        // myRef.setValue("Jersey City");
 
 
 
-        return rootView;
+
 
 
 
