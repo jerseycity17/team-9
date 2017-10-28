76d8651e59e9e18a59fdc08d275bbcf56dd3175e
HunterBartholomew <hbarthol@stevens.edu>
2017-10-27 20:31:50 -0400

    fixed bootstrap again

---
+++ Front End/Bootstrap/js/dashboard_data.js
@@ -0,0 +1,17 @@
+/*global angular */
+var usersApp = angular.module('usersApp', ['ngRoute']);
+usersApp.controller('usersListCrtl', function ($scope, $http) {
+    "use strict";
+    
+    var userData = {
+            "user_id": "hbarthol@stevens.edu",
+            "password": "12345",
+            "create_date": "3/25/2017",
+            "name": "Hunter Bartholomew",
+            "age": 20
+        };
+        
+    $scope.user_name = userData["name"]
+
+	//});
+});
