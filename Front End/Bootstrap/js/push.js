2bcee54da5caf43e34ce237fa0ab8ed007b436c0
HunterBartholomew <hbarthol@stevens.edu>
2017-10-28 10:28:59 -0400

    still trying to push to db :(

--- Front End/Bootstrap/js/push.js
+++ Front End/Bootstrap/js/push.js
@@ -1,5 +1,3 @@
-// Initialize Firebase
-// TODO: Replace with your project's customized code snippet
 var config = {
     apiKey: "AIzaSyC8NkmJKsy_P4ERtWiBo_hnObKErwfAfpk",
     authDomain: "teamnine-40084.firebaseapp.com",
@@ -8,10 +6,8 @@ var config = {
     messagingSenderId: "1056108808174",
 };
 firebase.initializeApp(config);
-var database = firebase.database();
-firebase.initializeApp(config);
 function writeUserData() {
-  firebase.database().ref('users/' + userId).set({
+  firebase.database().ref('data/alerts/').set({
     alerts: "TEST ALERT"
   });
-}
+};
