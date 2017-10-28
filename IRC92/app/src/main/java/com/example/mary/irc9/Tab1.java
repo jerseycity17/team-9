53ff23a45f2735d3478e4caa2fdce2f630fa802e
Mary-Yan <maryyan2121@gmail.com>
2017-10-28 00:18:04 -0400

    First Android Studio push

---
+++ IRC92/app/src/main/java/com/example/mary/irc9/Tab1.java
@@ -0,0 +1,22 @@
+package com.example.mary.irc9;
+
+/**
+ * Created by Mary on 10/28/17.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab1 extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+        return rootView;
+    }
+}
